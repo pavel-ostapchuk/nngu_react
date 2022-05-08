331c2f4b01e4a0673d72f5cce34153ebc2b0e5ae
pvostapchuk <PVOstapchuk@sberbank.ru>
2022-05-08 18:47:52 +0300

    lesson

--- src/components/MainPage/index.tsx
+++ src/components/MainPage/index.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import Footer from '../Footer';
 import Header from '../Header';
-import ItemsList from '../ItemsList';
+import Footer from '../Footer';
 import style from './MainPage.module.scss';
+import ItemsList from '../ItemsList';
 
 const MainPage = () => {
-  // ================================================
-
-  // Пример использования JSX:
-  //   const isTrue = true;
-  //   const text1 = 'text1';
-  //   const text2 = 'text2';
-
-  //   return <div className={style.main_page}>{isTrue ? text1 : text2}</div>;
-
-  // ================================================
-
   return (
-    <div className={style.page_wrapper}>
+    <div className={style.main_page}>
       <Header />
       <ItemsList />
       <Footer />
