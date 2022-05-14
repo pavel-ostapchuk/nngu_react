d3ba90b06a7254facd6b52d5969c84c00805052a
pvostapchuk <PVOstapchuk@sberbank.ru>
2022-05-14 09:17:14 +0300

    delete src

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './assets/scss/normalize.scss';
-import MainPage from './components/MainPage';
-
-const App = () => {
-  return <MainPage />;
-};
-
-export default App;
