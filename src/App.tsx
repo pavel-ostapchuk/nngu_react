6021fa52973d6fd9e6de7e33646e33cca9d53124
pvostapchuk <PVOstapchuk@sberbank.ru>
2022-05-21 00:22:07 +0300

    template init

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import './assets/scss/normalize.scss';
-import UsersPage from './entities/UsersPage';
-import PageLayout from './layouts/MainLayout';
+import PageLayout from './layouts/PageLayout';
 
 const App = () => {
-  return (
-    <PageLayout>
-      <UsersPage />
-    </PageLayout>
-  );
+  return <div />;
 };
 
 export default App;
