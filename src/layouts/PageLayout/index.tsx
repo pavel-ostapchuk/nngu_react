56f087aa000a2bbedad33713d3539da8b8d8a587
pvostapchuk <PVOstapchuk@sberbank.ru>
2022-05-28 08:29:28 +0300

    upd template

--- src/layouts/PageLayout/index.tsx
+++ src/layouts/PageLayout/index.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from 'react';
 import Footer from '../../shared/Footer';
 import Header from '../../shared/Header';
-import Sidebar from '../../shared/Sidebar';
 import style from './PageLayout.module.scss';
 
 interface IPageLayout {
@@ -11,7 +10,6 @@ interface IPageLayout {
 const PageLayout: FC<IPageLayout> = ({ children }) => {
   return (
     <div className={style.page_wrapper}>
-      <Sidebar />
       <div className={style.section_wrapper}>
         <Header />
         <div className={style.content_wrapper}>{children}</div>
