3cc8a3108fd1d81d13c15d97e7ea9a535700b1e4
pvostapchuk <PVOstapchuk@sberbank.ru>
2022-05-30 10:01:30 +0300

    delete src

--- src/api/constats.ts
+++
@@ -1,1 +0,0 @@
-export const usersUrl = 'https://jsonplaceholder.typicode.com/users';
--- src/shared/Header/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import style from './Header.module.scss';
-
-const Header = () => {
-  return <div className={style.page_header}>Шапка сайта</div>;
-};
-
-export default Header;
